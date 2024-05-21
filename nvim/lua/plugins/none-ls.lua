b0c98a82b6b7f01de42f4712e5227488d67225f8
Max Copeland <maxcope@amazon.com>
2024-05-21 09:16:33 -0700

    Add linters/formatters for tf, python. Tree-sitter auto-install for detected langs

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -4,10 +4,17 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
+        -- lua
 				null_ls.builtins.formatting.stylua,
+        -- python
         null_ls.builtins.diagnostics.pylint,
         null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.tflint
+        null_ls.builtins.formatting.isort,
+        -- terraform
+        null_ls.builtins.diagnostics.terraform_validate,
+        null_ls.builtins.formatting.terraform_fmt,
+        null_ls.builtins.diagnostics.tfsec,
+        null_ls.builtins.diagnostics.trivy
 			},
 		})
 
