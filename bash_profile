d2d0eee1e78c283b76ad1699230a8027d549aad7
Max Copeland <maxcope@amazon.com>
2024-05-21 08:44:54 -0700

    Working lsp and formatting

--- bash_profile
+++ bash_profile
@@ -6,5 +6,8 @@ alias ls='ls -lh'
 alias python='python3'
 export BASH_SILENCE_DEPRECATION_WARNING=1
 
+# Adding this HomeBrew path to PATH for tmux
+# https://github.com/tmux-plugins/tpm/issues/67
+
 # Amazon Q post block. Keep at the bottom of this file.
 [[ -f "${HOME}/Library/Application Support/amazon-q/shell/bash_profile.post.bash" ]] && builtin source "${HOME}/Library/Application Support/amazon-q/shell/bash_profile.post.bash"
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,12 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
+        ensure_installed = {
+          "lua_ls",
+          "pyright",
+          "terraformls",
+          "tflint"
+          }
         })
       end
   },
@@ -18,6 +23,19 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.terraformls.setup({})
+      lspconfig.tflint.setup({})
+
+      vim.cmd([[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]])
+      vim.cmd([[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]])
+      vim.cmd([[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]])
+      vim.cmd([[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]])
+      vim.cmd([[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]])
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gD', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
