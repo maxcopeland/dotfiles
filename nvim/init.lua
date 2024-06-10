c668369181491406f361eb31c4187da0c2cdd0b2
Max Copeland <maxcope@amazon.com>
2024-06-10 16:25:08 -0700

    Filter ansi escape in terraform keybinding

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,3 +14,21 @@ vim.opt.rtp:prepend(lazypath)
 require("vim-options")
 require("lazy").setup("plugins")
 
+local opts = {}
+
+require('lspconfig').terraformls.setup{}
+require('lspconfig').tflint.setup{}
+
+vim.cmd([[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]])
+vim.cmd([[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]])
+vim.cmd([[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]])
+vim.cmd([[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]])
+vim.cmd([[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]])
+
+vim.cmd([[let g:terraform_fmt_on_save=1]])
+vim.cmd([[let g:terraform_align=1]])
+
+vim.keymap.set("n", "<leader>ti", ":!terraform init 2>&1 | sed -r 's/\\x1B\\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g'<CR>", opts)
+vim.keymap.set("n", "<leader>tv", ":!terraform validate 2>&1 | sed -r 's/\\x1B\\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g'<CR>", opts)
+vim.keymap.set("n", "<leader>tp", ":!terraform plan 2>&1 | sed -r 's/\\x1B\\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g'<CR>", opts)
+vim.keymap.set("n", "<leader>taa", ":!terraform apply --auto-approve 2>&1 | sed -r 's/\\x1B\\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g'<CR>", opts)
